63398aff7d1983437188b6c5f4b2328ae5959761
Sherief Shawky <mcrakens@gmail.com>
2015-11-01 11:51:53 +0200

    Fixes the db search type name

--- src/main/java/com/scaffy/product/restful/Searchable.java
+++ src/main/java/com/scaffy/product/restful/Searchable.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 public @interface Searchable {
 	
 	public enum DBSearchType {
-		IGNORE(""), JPA("jpa");
+		IGNORE(""), JPA("db_jpa");
 		
 		public String value;
 		
